6ee9792f85032ff9b0de5b76643fb466f92b58c0
mahaloz <zion@zionbasque.com>
2024-01-09 22:44:47 -0700

    Bump to be LibBS 0.17.0 compliant

--- dailalib/api/ai_api.py
+++ dailalib/api/ai_api.py
@@ -93,7 +93,9 @@ class AIAPI:
 
                 # we must have a UI if we have no func
                 if function is None:
-                    function = ai_api._dec_interface.functions[ai_api._dec_interface.active_context().addr]
+                    function = ai_api._dec_interface.art_lifter.lower(
+                        ai_api._dec_interface.functions[ai_api._dec_interface.active_context().addr]
+                    )
 
                 # get new text with the function that is present
                 if dec_text is None:
