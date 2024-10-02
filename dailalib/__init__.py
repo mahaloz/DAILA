b120c5636e17cf5b6d01a8ecd1c6340f81cc00e7
mahaloz <zion@zionbasque.com>
2024-10-01 19:40:11 -0700

    Use local model cost for LiteLLM

--- dailalib/__init__.py
+++ dailalib/__init__.py
@@ -1,5 +1,10 @@
 __version__ = "3.9.0"
 
+import os
+# stop LiteLLM from querying at all to the remote server
+# https://github.com/BerriAI/litellm/blob/4d29c1fb6941e49191280c4fd63961dec1a1e7c5/litellm/__init__.py#L286C20-L286C48
+os.environ["LITELLM_LOCAL_MODEL_COST_MAP"] = "True"
+
 from .api import AIAPI, LiteLLMAIAPI
 
 from dailalib.llm_chat import get_llm_chat_creator
