c24abddfbac3394055b2aa5429889e8249cda79d
mahaloz <zion@zionbasque.com>
2025-01-27 22:42:22 -0700

    Some fixes

--- dailalib/api/litellm/litellm_api.py
+++ dailalib/api/litellm/litellm_api.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from typing import Optional
 import os
+import logging
 
 import tiktoken
 
+from . import DEFAULT_MODEL, LLM_COST, OPENAI_MODELS
 from ..ai_api import AIAPI
 from .configuration import DAILAConfig
 from .config_dialog import DAILAConfigDialog
@@ -10,52 +13,94 @@ from .config_dialog import DAILAConfigDialog
 active_model = None
 active_prompt_style = None
 
-import logging 
 _l = logging.getLogger(__name__)
+
 
 class LiteLLMAIAPI(AIAPI):
     prompts_by_name = []
-    DEFAULT_MODEL = "gpt-4o"
-    OPENAI_MODELS = {"gpt-4", "gpt-4o", "gpt-4-turbo", "gpt-3.5-turbo", "o1-mini", "o1-preview"}
-    # TODO: How can I get this MODEL_TO_TOKENS in the future, without hardcopy to `configuration`
-    MODEL_TO_TOKENS = {
-        # TODO: update the token values for o1
-        "o1-mini": 8_000,
-        "o1-preview": 8_000,
-        "gpt-4o": 8_000,
-        "gpt-4o-mini": 16_000,
-        "gpt-4-turbo": 128_000,
-        "claude-3-5-sonnet-20240620": 200_000,
-        "gemini/gemini-pro": 12_288,
-        "vertex_ai_beta/gemini-pro": 12_288,
-        # perplex is on legacy mode :( 
-        "perplexity/llama-3.1-sonar-small-128k-online": 127_072,
-        "perplexity/llama-3.1-sonar-medium-128k-online": 127_072,
-        "perplexity/llama-3.1-sonar-large-128k-online": 127_072,
-        "sonar-pro": 127_072,
-        "sonar": 127_072,
-    }
 
-    def load_config(self):
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        model: str = DEFAULT_MODEL,
+        prompt_style: str = "few-shot",
+        prompts: Optional[list] = None,
+        fit_to_tokens: bool = False,
+        chat_use_ctx: bool = True,
+        chat_event_callbacks: Optional[dict] = None,
+        custom_endpoint: Optional[str] = None,
+        custom_model: Optional[str] = None,
+        use_config: bool = True,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        # default values
+        self._api_key = None
+        # default to openai api key if not provided
+        self.api_key = api_key or os.getenv("OPENAI_API_KEY")
+        self.model = model
+        self.prompt_style = prompt_style
+        self.fit_to_tokens = fit_to_tokens
+        self.chat_use_ctx = chat_use_ctx
+        self.chat_event_callbacks = chat_event_callbacks or {"send": None, "receive": None}
+        self.custom_endpoint = custom_endpoint
+        self.custom_model = custom_model
+        self.config = DAILAConfig()
+        if use_config:
+            loaded = self.load_or_create_config()
+            if loaded:
+                _l.info("Loaded config file from %s", self.config.save_location)
+
+        # delay prompt import
+        from .prompts import PROMPTS
+        prompts = prompts + PROMPTS if prompts else PROMPTS
+        self.prompts_by_name = {p.name: p for p in prompts}
+
+        # update the globals (for threading hacks)
+        global active_model, active_prompt_style
+        active_model = self.model
+        active_prompt_style = self.prompt_style
+
+    def load_or_create_config(self, new_config=None) -> bool:
+        if new_config:
+            self.config = new_config
+            self.config.save()
+
+        if self.config.save_location and not Path(self.config.save_location).exists():
+            return False
+
+        # load the config
         self.config.load()
         self.model = self.config.model
         self.api_key = self.config.api_key
         self.prompt_style = self.config.prompt_style
-        self.custom_endpoint = self.config.custom_endpoint
-        self.custom_model = self.config.custom_model
+        if self.config.custom_endpoint:
+            self.custom_endpoint = self.config.custom_endpoint
+        if self.config.custom_model:
+            self.custom_model = self.config.custom_model
         # update the globals (for threading hacks)
         self._set_model(self.model)
         self._set_prompt_style(self.prompt_style)
+        return True
+
+    def __dir__(self):
+        return list(super().__dir__()) + list(self.prompts_by_name.keys())
+
+    def __getattribute__(self, item):
+        # this is how we can access the prompt functions
+        if item in object.__getattribute__(self, "prompts_by_name"):
+            prompt_obj: "Prompt" = self.prompts_by_name[item]
+            prompt_obj.ai_api = self
+            return prompt_obj.query_model
+        else:
+            return object.__getattribute__(self, item)
 
     @property
     def api_key(self):
-        # if this key is load from config, just skip the next steps 
-        if self.is_load_from_config:
-            return self._api_key
-        
         if not self._api_key or self.model is None:
             return None
-        elif self.model in self.OPENAI_MODELS:
+        elif self.model in OPENAI_MODELS:
             return os.getenv("OPENAI_API_KEY", None)
         elif "claude" in self.model:
             return os.getenv("ANTHROPIC_API_KEY", None)
@@ -73,7 +118,7 @@ class LiteLLMAIAPI(AIAPI):
         self._api_key = value
         _l.info(f"API key set to {self.model}")
         if self._api_key and self.model is not None:
-            if self.model in self.OPENAI_MODELS:
+            if self.model in OPENAI_MODELS:
                 os.environ["OPENAI_API_KEY"] = self._api_key
             elif "claude" in self.model:
                 os.environ["ANTHROPIC_API_KEY"] = self._api_key
@@ -90,10 +135,10 @@ class LiteLLMAIAPI(AIAPI):
     def custom_model(self):
         return self._custom_model
 
-    @custom_model.setter 
+    @custom_model.setter
     def custom_model(self, value):
-        custom_model = value
-        if not custom_model.strip():
+        custom_model = value.strip() if isinstance(value, str) else None
+        if not custom_model:
             self._custom_model = None
             _l.info(f"Custom model selection cleared, or not in use")
             return
@@ -103,11 +148,11 @@ class LiteLLMAIAPI(AIAPI):
     @property
     def custom_endpoint(self):
         return self._custom_endpoint
-    
+
     @custom_endpoint.setter
     def custom_endpoint(self, value):
-        custom_endpoint = value
-        if not custom_endpoint.strip():
+        custom_endpoint = value.strip() if isinstance(value, str) else None
+        if not custom_endpoint:
             self._custom_endpoint = None
             _l.info(f"Custom endpoint disabled, defaulting to online API")
             return
@@ -117,69 +162,6 @@ class LiteLLMAIAPI(AIAPI):
             return
         self._custom_endpoint = custom_endpoint.strip()
         _l.info(f"Custom endpoint set to {self._custom_endpoint}")
-
-    # replacement strings for API calls
-    def __init__(
-        self,
-        api_key: Optional[str] = None,
-        model: str = DEFAULT_MODEL,
-        prompt_style: str = "few-shot",
-        prompts: Optional[list] = None,
-        fit_to_tokens: bool = False,
-        chat_use_ctx: bool = True,
-        chat_event_callbacks: Optional[dict] = None,
-        custom_endpoint: Optional[str] = None,
-        custom_model: Optional[str] = None,
-        **kwargs
-    ):
-        super().__init__(**kwargs)
-        
-        self.config = DAILAConfig()
-
-        self.is_load_from_config = False 
-        
-        # Check if the config file exists. Preferably load the config file 
-        if self.config.save_location.exists():
-            self.is_load_from_config = True 
-            self.load_config()
-        else: 
-            # default values 
-            self._api_key = None
-            # default to openai api key if not provided
-            self.api_key = api_key or os.getenv("OPENAI_API_KEY")
-            self.model = model
-            self.prompt_style = prompt_style
-            self.fit_to_tokens = fit_to_tokens
-            self.chat_use_ctx = chat_use_ctx
-            self.chat_event_callbacks = chat_event_callbacks or {"send": None, "receive": None}
-            self.custom_endpoint = custom_endpoint
-            self.custom_model = custom_model
-
-        self.fit_to_tokens = fit_to_tokens
-        self.chat_use_ctx = chat_use_ctx
-        self.chat_event_callbacks = chat_event_callbacks or {"send": None, "receive": None}
-
-        # delay prompt import
-        from .prompts import PROMPTS
-        prompts = prompts + PROMPTS if prompts else PROMPTS
-        self.prompts_by_name = {p.name: p for p in prompts}
-
-        # update the globals (for threading hacks)
-        global active_model, active_prompt_style
-        active_model = self.model
-        active_prompt_style = self.prompt_style
-
-    def __dir__(self):
-        return list(super().__dir__()) + list(self.prompts_by_name.keys())
-
-    def __getattribute__(self, item):
-        # this is how we can access the prompt functions
-        if item in object.__getattribute__(self, "prompts_by_name"):
-            prompt_obj: "Prompt" = self.prompts_by_name[item]
-            prompt_obj.ai_api = self
-            return prompt_obj.query_model
-        else:
-            return object.__getattribute__(self, item)
 
     def query_model(
         self,
@@ -254,25 +236,10 @@ class LiteLLMAIAPI(AIAPI):
     @staticmethod
     def llm_cost(model_name: str, prompt_tokens: int, completion_tokens: int) -> float | None:
         # these are the $ per million tokens
-        COST = {
-            "gpt-4o": {"prompt_price": 2.5, "completion_price": 10},
-            "gpt-4o-mini": {"prompt_price": 0.150, "completion_price": 0.600},
-            "gpt-4-turbo": {"prompt_price": 10, "completion_price": 30},
-            "claude-3.5-sonnet-20240620": {"prompt_price": 3, "completion_price": 15},
-            "gemini/gemini-pro": {"prompt_price": 0.150, "completion_price": 0.600},
-            "vertex_ai_beta/gemini-pro": {"prompt_price": 0.150, "completion_price": 0.600},
-            # perplex is on legacy mode not available from 02/22/25:(
-            "perplexity/llama-3.1-sonar-small-128k-online": {"prompt_price": 0.150, "completion_price": 0.600},
-            "perplexity/llama-3.1-sonar-large-128k-online": {"prompt_price": 0.150, "completion_price": 0.600},
-            "perplexity/llama-3.1-sonar-huge-128k-online": {"prompt_price": 0.150, "completion_price": 0.600},
-            # introduced the new sonar-pro/sonar 
-            "sonar": {"prompt_price": 0.150, "completion_price": 0.600},
-            "sonar-pro": {"prompt_price": 0.150, "completion_price": 0.600},
-        }
-        if model_name not in COST:
+        if model_name not in LLM_COST:
             return None
 
-        llm_price = COST[model_name]
+        llm_price = LLM_COST[model_name]
         prompt_price = (prompt_tokens / 1000000) * llm_price["prompt_price"]
         completion_price = (completion_tokens / 1000000) * llm_price["completion_price"]
 
@@ -303,11 +270,7 @@ class LiteLLMAIAPI(AIAPI):
         dialog = DAILAConfigDialog(self.config)
         new_config = dialog.config_dialog_exec()
         if new_config:
-            # apply the new config
-            self.is_load_from_config = True
-            self.config = new_config
-            self.config.save()
-            self.load_config()
+            self.load_or_create_config(new_config=new_config)
             self._dec_interface.info("DAILA Settings applied.")
         else:
             self._dec_interface.error("DAILA Settings not applied.")
