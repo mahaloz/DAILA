9054e4a28eeb901cfc1e7ba90f6d857c0ec92cbf
Zion Leonahenahe Basque <zion@zionbasque.com>
2025-03-11 07:26:09 -0700

    Remove deprecated Binja API (#86)

--- dailalib/__init__.py
+++ dailalib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.16.1"
+__version__ = "3.16.2"
 
 import os
 # stop LiteLLM from querying at all to the remote server
--- dailalib/llm_chat/__init__.py
+++ dailalib/llm_chat/__init__.py
@@ -14,9 +14,8 @@ def get_llm_chat_creator(ai_api: AIAPI) -> callable:
     add_llm_chat_to_ui = lambda *args, **kwargs: None
     if current_decompiler == IDA_DECOMPILER:
         from dailalib.llm_chat.ida import add_llm_chat_to_ui
-    elif current_decompiler == BINJA_DECOMPILER:
-        from dailalib.llm_chat.binja import add_llm_chat_to_ui
     else:
+        # TODO we had Binja support, but needed to disable until https://github.com/mahaloz/DAILA/issues/85
         _l.warning(f"LLM Chat not supported for decompiler %s", current_decompiler)
 
     def llm_chat_creator_wrapper(*args, **kwargs):
